2b1014184f64666fe26f79351359f1f6c3c3bbb4
edu210 <edu210@gmail.com>
2022-09-23 20:05:24 -0500

    se actualiza

---
+++ suma.cpp
@@ -0,0 +1,26 @@
+
+//Nombre: Edu Hurtado Arce
+//curso: 1"A"
+//docentes: stalin farncis
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float e,h,a;
+	cout<<"Ingrese 2 numero e h :";
+ 
+	cin>>e>>h;
+ 
+	a=e+h;
+ 
+	cout<<"El resutado es : ";
+ 
+	cout<<a<<endl;
+	return 0;
+
+}
